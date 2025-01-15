9c40dfa108596694f9a9e26a7f6adcd2b7346750
moises2872024 <moises.montt287@gmail.com>
2025-01-15 14:39:08 +0000

    avance

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
+export const Navbar = ({ favorites = [], removeFavorite }) => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		<nav className="navbar bg-dark text-light px-3">
+			<Link to="/" className="navbar-brand text-light">
+				Star Wars Blog
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<div className="dropdown">
+				<button
+					className="btn btn-secondary dropdown-toggle"
+					type="button"
+					id="favoritesDropdown"
+					data-bs-toggle="dropdown"
+					aria-expanded="false"
+				>
+					Favorites ({favorites.length})
+				</button>
+				<ul className="dropdown-menu dropdown-menu-end" aria-labelledby="favoritesDropdown">
+					{favorites.length === 0 ? (
+						<li className="dropdown-item text-muted">No favorites added</li>
+					) : (
+						favorites.map((fav, index) => (
+							<li key={index} className="dropdown-item d-flex justify-content-between align-items-center">
+								<Link to={`/info/${fav.uid}`} className="text-decoration-none">
+									{fav.name} ({fav.type})
+								</Link>
+								<button
+									className="btn btn-danger btn-sm"
+									onClick={() => removeFavorite(fav)}
+								>
+									Remove
+								</button>
+							</li>
+						))
+					)}
+				</ul>
 			</div>
 		</nav>
 	);
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,37 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import { Info } from "./views/info";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
 
-//create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+  const basename = process.env.BASENAME || "";
+  const [favorites, setFavorites] = useState([]);
 
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+  const removeFavorite = (item) => {
+    setFavorites(favorites.filter((fav) => fav.uid !== item.uid));
+  };
+
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar favorites={favorites} removeFavorite={removeFavorite} />
+          <Routes>
+            <Route path="/" element={<Home setFavorites={setFavorites} favorites={favorites} />} />
+            <Route path="/info/:id" element={<Info />} />
+            <Route path="*" element={<h1>Not found!</h1>} />
+          </Routes>
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
